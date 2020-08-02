9a392db4a7709ba333885c419860a6d2e34ca665
maha139 <mahapattabiraman@gmail.com>
2020-08-02 11:49:02 +0530

    Add files via upload

---
+++ whatsyourname.py
@@ -0,0 +1,9 @@
+#Task4
+# Get firstname and last name and see if less than 10 characters
+
+firstname = input("enter first name")
+lastname = input("enter last name")
+print(firstname)
+print(lastname)
+print ("Hello {} {} You just delved into Python".format(firstname,lastname))
+
