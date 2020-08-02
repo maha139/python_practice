ddd1e60d68da635c11cde853564d7198692d90c4
maha139 <mahapattabiraman@gmail.com>
2020-08-02 11:36:02 +0530

    Add files via upload

---
+++ Volumeofsphere.py
@@ -0,0 +1,18 @@
+#Task2:
+#Tasks:
+
+#task2:
+
+#1/3 * pi * r2 * h volume cone
+#pi ==> static
+#r ==> get it from user (input) ==> float
+#h ===> input ===> integer
+
+#Final ==> integer
+
+pi = 3.14
+r = float(input("radius"))
+h = int(input("height"))
+volume= 1/3 *pi * (r**2)*h
+x = int(volume)
+print(x)
