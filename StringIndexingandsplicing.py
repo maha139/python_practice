349af87693205befcf20f2b9084b8e53735a7f29
maha139 <mahapattabiraman@gmail.com>
2020-08-02 11:08:49 +0530

    Add files via upload

---
+++ StringIndexingandsplicing.py
@@ -0,0 +1,21 @@
+a = "computer_science"
+
+print(a[-2:0:-1])  #cneics_retupmo
+print(a[5:-5:5])  #tc
+print(a[-5:5:-5]) #ie
+print(a[5:-5:-5]) #empty
+print(a[3:9:-1]) #empty
+print(a[9:-10:-2]) #sre
+print(a[2:-4:-4])  #empty
+print(a[-4:2:-2])  #ec_eu
+print(a[0:-1:-3])  #empty
+print(a[-3:3:6])   #empty
+print(a[::-3])     #eesepc
+print(a[::-2])    #enisrtpo
+print(a[2:-2:-2])  #empty
+print(a[2:-2:2])   #mue_ce
+print(a[5:-5:5])   #tc
+print(a[3:-3:3])   #pese
+print(a[::2])      #cmue_cec
+print(a[::-5])     #ectc
+
